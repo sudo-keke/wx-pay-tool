2ee90355951e84f3a55eae69f65e10f4d734105f
yq <yan_qiang_1@163.com>
2019-08-02 11:02:57 +0800

    first commit

---
+++ src/main/java/com/yq/wx/WxPayToolApplication.java
@@ -0,0 +1,13 @@
+package com.yq.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxPayToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxPayToolApplication.class, args);
+    }
+
+}

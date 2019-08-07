2bb3a9a1f312de07d5a73adffd1cf0528ad004bf
yq <yan_qiang_1@163.com>
2019-08-07 14:21:46 +0800

    查询订单

--- src/main/java/com/yq/wx/pay/Unifiedorder.java
+++ src/main/java/com/yq/wx/pay/Unifiedorder.java
@@ -21,6 +21,10 @@ import java.util.concurrent.ConcurrentHashMap;
  * @Author :  Yanqinag
  * @Date :  2019-08-07 11:03
  * @Description :  微信支付
+ *
+ *  1.支付
+ *  2.结果通知
+ *  3.查询订单
  */
 public class Unifiedorder {
 
@@ -49,6 +53,7 @@ public class Unifiedorder {
     public BaseResult unifiedorder(HttpServletRequest request, String appId, String mchId, String body, String outTradeNo, String totalFee, String openId, String notifyUrl)  {
 
         BaseResult baseResult = new BaseResult();
+        //total_fee单位为分,将元转为分，或者在外面就处理了
         String total = new BigDecimal(totalFee).multiply(new BigDecimal("100")).stripTrailingZeros().toPlainString();
         //构造请求参数 不需要此时设置sign签名，wxPay.unifiedOrder()接口 通过配置类自动配置
         ConcurrentHashMap<String, String> requestMap = new ConcurrentHashMap<>(20);
@@ -126,5 +131,75 @@ public class Unifiedorder {
      */
     public String setXML(String return_code, String return_msg) {
         return "<xml><return_code><![CDATA[" + return_code + "]]></return_code><return_msg><![CDATA[" + return_msg + "]]></return_msg></xml>";
+    }
+
+
+    /**
+     * @Author : Yanqiang
+     * @Date : 2019/7/22
+     * @Param : [outTradeNo] 订单编号
+     * @return : void
+     * @Description : 查询订单
+     *  在微信支付通知不到时，主动查询订单状态，以便及时更新用户支付信息
+     *  建议在支付成功后调用此接口，不建议在查询用户订单时调用
+     *  另，应该记录用户支付信息详细
+     *
+     * return_code 是用来判断通信状态的，在“结果通知”时必为 SUCCESS；
+     * result_code 是用来判断业务结果的，指一次调用接口或回调的动作是否如愿执行成功。如“关闭订单”时关闭成功为 SUCCESS，因参数配置错误、找不到订单号、订单状态不允许关闭等其它关闭失败的情况为 FAIL；
+     * trade_state 是用来判断交易状态的，“交易”是指微信支付订单。
+     * 在调用“统一下单”时是未支付状态，根本没有支付成功的可能。
+     * 在异步收到“结果通知”时，不要相信文档去判断 result_code，应调用“查询订单”，并判断 trade_state。
+     */
+    private void orderquery(String appId, String mchId, String outTradeNo){
+        //构造请求参数
+        ConcurrentHashMap<String, String> requestMap = new ConcurrentHashMap<>(8);
+        requestMap.put("appid", appId);
+        requestMap.put("mch_id", mchId);
+        requestMap.put("nonce_str", WXPayUtil.generateNonceStr());
+        requestMap.put("out_trade_no", outTradeNo);
+        try {
+            //MyWXPayConfig 配置类；useSandbox 沙箱环境 （false为正式/true为沙箱环境）
+            WXPay wxPay = new WXPay(new MyWXPayConfig("online"), null, true, false);
+            Map<String, String> refund = wxPay.orderQuery(requestMap, 6 * 1000, 8 * 1000);
+            if (refund.get("return_code").equals("SUCCESS")) {
+                //以下只在return_code为SUCCESS的时候有返回
+
+                if (refund.get("result_code").equals("SUCCESS")){
+                    //以下只在return_code为SUCCESS的时候有返回
+
+                    if(refund.get("trade_state").equals("SUCCESS")){
+                        //支付成功,说明该行数据为一笔支付成功的订单，更新用户订单信息
+
+                    }else if (refund.get("trade_state").equals("REFUND")){
+                        //转入退款，说明该行数据为一笔发起退款成功的退款单，说明退款成功，进行订单退款处理的状态变更
+
+                    }else if(refund.get("trade_state").equals("NOTPAY")){
+                        //未支付，删除订单，如果用户再次发起购买，重新下单
+
+                    }else if(refund.get("trade_state").equals("CLOSED")){
+                        //已关闭，删除订单，如果用户再次发起购买，重新下单
+
+                    }else if(refund.get("trade_state").equals("USERPAYING")){
+                        //用户支付中，延迟一会儿，再次查询
+
+                    }else if(refund.get("trade_state").equals("PAYERROR")){
+                        //支付失败(其他原因，如银行返回失败) 删除订单，如果用户再次发起购买，重新下单
+
+                    }else {
+                        //其他原因
+
+                    }
+                }else {
+                    refund.get("err_code");
+                    refund.get("err_code_des");
+                }
+            }else {
+                //return_code为FAIL,只返回这个两个字段
+                refund.get("return_code");
+                refund.get("return_msg");
+            }
+        }catch (Exception e){
+            e.getStackTrace();
+        }
     }
 }
